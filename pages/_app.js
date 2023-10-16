fd385422fd9d7bd074838539072a105e98785bd9
Stephan Lippert <stephman1984@gmail.com>
2023-10-16 13:21:28 +0200

    add swr-config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
-import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <GlobalStyle />
+    <SWRConfig
+      value={{
+        fetcher: async (...args) => {
+          const response = await fetch(...args);
+          if (!response.ok) {
+            throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+          }
+          return await response.json();
+        },
+      }}
+    >
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
