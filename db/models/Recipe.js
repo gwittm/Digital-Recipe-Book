1a41fd9e111135c6fe0dfe1765267369bb285a3a
Stephan Lippert <stephman1984@gmail.com>
2023-10-19 09:43:29 +0200

    fixed

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -6,7 +6,7 @@ const recipeSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String },
   course: { type: String, required: true },
-  time: { type: String },
+  time: { type: String, required: true },
   yield: { type: String },
   ingredients: { type: Array },
   steps: { type: String, required: true },
