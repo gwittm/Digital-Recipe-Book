49856b4a69339e6bf882b0f11994e36cfa735f8a
Gabriele Wittmann <gabrielewittmann@web.de>
2023-11-18 17:19:26 +0100

    create and render searchbar without functionality

---
+++ components/Search/SearchBar.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export default function SearchBar({ handleSearch }) {
+  return (
+    <StyledForm>
+      <StyledLabel htmlFor="search" />
+      <StyledInput
+        type="text"
+        id="search"
+        name="search"
+        required
+        minLength="0"
+        maxLength="14"
+        size="10"
+        placeholder="Search from your recipes"
+        onChange={handleSearch}
+      />
+    </StyledForm>
+  );
+}
+
+const StyledForm = styled.form`
+  display: grid;
+  padding: 20px;
+  margin: 10px;
+  border: solid black 1px;
+  background-color: lightblue;
+`;
+
+const StyledLabel = styled.label`
+  padding-bottom: 10px;
+`;
+
+const StyledInput = styled.input`
+  height: 4vh;
+`;
