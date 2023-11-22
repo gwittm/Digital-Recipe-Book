55af19acf32fd7b76b9f7699cf019ca5d50d8463
Gabriele Wittmann <gabrielewittmann@web.de>
2023-11-22 09:22:31 +0100

    individual delete of ingredients not working, error at formularaddrecipe

--- components/Formular/FormularAddRecipe.js
+++ components/Formular/FormularAddRecipe.js
@@ -11,8 +11,8 @@ import FormularIngredients from "./FormularIngredients.js";
 
 export default function RecipeForm({ onSubmit, formName, defaultData }) {
   const [ingredients, setIngredients] = useState(defaultData.ingredients || []);
-  /*  const [ingredients, setIngredients] = useState([]); */
-
+  /*   const [ingredients, setIngredients] = useState([]);
+   */
   function handleAddIngredient(newIngredient) {
     setIngredients([...ingredients, newIngredient]);
   }
