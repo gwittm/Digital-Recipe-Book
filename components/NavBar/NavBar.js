cc9d2755c90a6749d14c69061e7537a4932e9c12
Stephan Lippert <stephman1984@gmail.com>
2023-11-22 21:02:48 +0100

    add some style

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styled from "styled-components";
 
 export default function NavBar() {
@@ -9,8 +8,8 @@ export default function NavBar() {
       </div>
       <div>
         <StyledAddLink href="/create">
-          + <br />
-          addNewRecipe
+          <span>+</span>
+          <span>AddNewRecipe</span>
         </StyledAddLink>
       </div>
       <div></div>
@@ -43,16 +42,52 @@ const baseLinkStyles = `
   display: flex;
   flex-direction: column;
   justify-content: center;
+  font-family: Georgia, "Times New Roman", Times, serif;
   align-items: center;
   color: white;
   text-decoration: none;
+  
+  @media screen and (min-width: 601px) {
+    font-size: 35px;
+  }
+
+  @media screen and (max-width: 600px) {
+    font-size: 20px;
+  }
 `;
 
 const StyledHomeLink = styled.a`
-  ${baseLinkStyles}/* Weitere Styles speziell für den Home-Link */
+  ${baseLinkStyles}
 `;
 
 const StyledAddLink = styled.a`
   ${baseLinkStyles}
+  position: relative;
   border: solid green 0.5rem;
+  margin-top: 0.3rem;
+  margin-bottom: 3rem;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+
+  span {
+    position: absolute;
+  }
+  span:first-child {
+    position: absolute;
+    color: green;
+    top: 1.2rem;
+    left: 1.5rem;
+    transform: translate(-50%, -50%);
+    font-size: 4.1rem;
+  }
+
+  span:last-child {
+    position: absolute;
+    color: green;
+    top: 100%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin-top: 1.3rem;
+  }
 `;
