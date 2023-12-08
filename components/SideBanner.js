76d1816192c9cc66ea7c6cf4bc2348ccbe50356c
Gabriele Wittmann <gabrielewittmann@web.de>
2023-12-08 12:49:01 +0100

    sidebar image

--- components/SideBanner.js
+++ components/SideBanner.js
@@ -5,8 +5,9 @@ export default function SideBanner() {
   return (
     <>
       <StyledSideBanner>
-        <Image
-          src="/public/nadine-primeau--bLkT8wGV0I-unsplash.jpg"
+        <StyledImageSidebar
+          src="/nadine-primeau--bLkT8wGV0I-unsplash.jpg"
+          alt="Image Side Banner"
           width={100}
           height={300}
         />
@@ -17,9 +18,14 @@ export default function SideBanner() {
 
 const StyledSideBanner = styled.div`
   position: fixed;
-  padding-top: 8px;
-  left: 500px;
+  left: 430px;
   height: 100%;
-  width: 100px;
-  padding: 20px;
+  width: 170px;
+  background-color: green;
+`;
+
+const StyledImageSidebar = styled(Image)`
+  object-fit: cover;
+  height: auto;
+  width: 170px;
 `;
