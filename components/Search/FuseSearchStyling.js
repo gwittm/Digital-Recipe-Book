d2dec969b79a8f014b11641c60383378234d1f60
Gabriele Wittmann <gabrielewittmann@web.de>
2023-12-17 20:01:35 +0100

    size searchBar fixed

--- components/Search/FuseSearchStyling.js
+++ components/Search/FuseSearchStyling.js
@@ -2,35 +2,29 @@ import styled from "styled-components";
 
 export const StyledForm = styled.form`
   display: flex;
-  justify-content: center;
-  padding: 10px;
+  justify-content: right;
+  padding-top: 10px;
   background-color: white;
-  width: 270px;
+  width: 280px;
   height: 12vh;
   position: fixed;
-  margin-left: 35px;
+  margin-left: 30px;
   margin-top: -20px;
-  padding-bottom: 20px;
 `;
 
 export const StyledLabel = styled.label`
-  padding: 5px;
+  /*   padding: 5px; */
 `;
 
 export const StyledInput = styled.input`
   position: fixed;
-  padding: 10px;
+  padding: 20px;
   height: 7vh;
-  width: 230px;
-  margin-left: 10px;
+  width: 270px;
   margin-top: 12px;
   color: var(--title-color);
   border: solid 3px var(--title-color);
   border-radius: 50px;
-
-  &:active {
-    border: solid 3px var(--header-color);
-  }
 `;
 
 export const StyledFuseUl = styled.ul`
